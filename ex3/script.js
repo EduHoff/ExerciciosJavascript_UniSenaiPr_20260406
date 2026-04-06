240476a3fdc3a6118f25b76133c37d9626cf2663
EduHoff <eduardohoffmanndocarmosilva@gmail.com>
2026-04-06 20:10:18 -0300

    Exercício 3 completo

---
+++ ex3/script.js
@@ -0,0 +1,13 @@
+const bntResultado = document.querySelector("#bntResultado")
+
+bntResultado.addEventListener("click", ()=>{
+
+    const peso = document.querySelector("#peso").valueAsNumber
+    const altura = document.querySelector("#altura").valueAsNumber
+
+    const imc = peso / altura**2
+
+
+    document.querySelector(".resultado").style.display = 'block';
+    document.querySelector("#resultado").innerHTML = `IMC: ${imc.toFixed(2)}`;
+})
