1316a075f156ab67945f61fdd09b619a06454ee7
EduHoff <eduardohoffmanndocarmosilva@gmail.com>
2026-04-06 19:34:27 -0300

    Exercício 1 completo

---
+++ ex1/script.js
@@ -0,0 +1,28 @@
+const bntResultado = document.querySelector("#bntResultado")
+
+bntResultado.addEventListener("click", ()=>{
+
+    const num1 = document.querySelector("#num1").valueAsNumber
+    const num2 = document.querySelector("#num2").valueAsNumber
+    const operacao = document.querySelector("#operacao").value
+
+    let resultado = undefined;
+    switch (operacao) {
+    case "soma":
+        resultado = num1 + num2;
+        break;
+    case "subtracao":
+        resultado = num1 - num2;
+        break;
+    case "multiplicacao":
+        resultado = num1 * num2;
+        break;
+    case "divisao":
+        resultado = num1 / num2;
+        break;
+    default:
+        resultado = null;
+    }
+
+    document.querySelector("#resultado").innerHTML = resultado;
+})
