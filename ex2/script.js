895fb135b8b930fa0f6a209419df1bf64811b00b
EduHoff <eduardohoffmanndocarmosilva@gmail.com>
2026-04-06 19:57:58 -0300

    Exercício 2 completo

---
+++ ex2/script.js
@@ -0,0 +1,18 @@
+const bntResultado = document.querySelector("#bntResultado")
+
+bntResultado.addEventListener("click", ()=>{
+
+    const celsius  = document.querySelector("#celsius").valueAsNumber
+
+    const fahrenheit = celsius * 1.8 + 32;
+    const kelvin = celsius + 273.15;
+
+    const divF = document.querySelector("#resultadoF");
+    const divK = document.querySelector("#resultadoK");
+
+    divF.style.display = 'block';
+    divK.style.display = 'block';
+
+    divF.innerHTML = `Fahrenheit: ${fahrenheit.toFixed(2)}°F`;
+    divK.innerHTML = `Kelvin: ${kelvin.toFixed(2)}K`;
+})
