0d8d05438359a8e8fda4b9847ef4b0333a3b1ba7
EduHoff <eduardohoffmanndocarmosilva@gmail.com>
2026-04-07 11:04:32 -0300

    Exercício 9 completo

---
+++ ex9/script.js
@@ -0,0 +1,27 @@
+const bntResultado = document.querySelector("#bntResultado")
+const count_index = document.querySelector("#count_index")
+const inputNum = document.querySelector("#num")
+const array = []
+
+bntResultado.addEventListener("click", () => {
+    const num = inputNum.valueAsNumber
+
+    array.push(num)
+    
+    if (array.length === 5) {
+        const media = array.reduce((a, b) => a + b, 0) / array.length
+        const maior = Math.max(...array)
+        const menor = Math.min(...array)
+
+        document.querySelector("#resultado").style.display = 'block';
+        document.querySelector("#resultado").innerHTML = `
+            <p>Média: ${media.toFixed(2)}</p>
+            <p>Maior Nota: ${maior}</p>
+            <p>Menor Nota: ${menor}</p>
+        `
+        bntResultado.disabled = true
+        return 
+    }
+
+    count_index.textContent = array.length + 1
+})
